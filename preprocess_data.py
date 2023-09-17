0c20e5457c361ceb5efe5dd4c8cbf66c09aa169d
Arunava Nag <arunavoster@gmail.com>
2023-09-17 16:37:07 -0700

    added preprocesing module

--- preprocess_data.py
+++ preprocess_data.py
@@ -6,6 +6,7 @@ from multiprocessing import Pool
 import multiprocessing as mp
 
 
+
 def compute_distance(streakline, odor_position, nearest_from_streakline):
     source = np.array([[0,0]])
     streakline = np.concatenate((streakline, source))
@@ -18,11 +19,12 @@ def find_streakline(df, dt):
     return eastwest, northsouth
 
 def process_file(folder_path, filename):
+    new_df=pd.DataFrame()
     df = pd.read_hdf(folder_path + filename)
     source = np.array([[0,0]])
     odor_position = np.array([[df.x[i], df.y[i]] for i in range(len(df))])
     distance_from_source = np.array([cdist(odor_position, source)]).flatten()
-    df['distance_from_source'] = distance_from_source
+    new_df['distance_from_source'] = distance_from_source
 
     dt = 0.3
     eastwest, northsouth = find_streakline(df, dt)
@@ -35,23 +37,27 @@ def process_file(folder_path, filename):
         wind_pos = np.vstack([eastwest, northsouth]).T
         compute_distance(wind_pos, odor_pos, nearest_from_streakline)
 
-    df['nearest_from_streakline'] = nearest_from_streakline[::-1]
+    # new_df['nearest_from_streakline'] = nearest_from_streakline[::-1]
+    nearest_from_streakline = nearest_from_streakline[::-1]
 
-    squared_difference = np.square(distance_from_source) - np.square(df.nearest_from_streakline)
+    squared_difference = np.square(distance_from_source) - np.square(nearest_from_streakline)
     squared_difference = np.abs(squared_difference)
-    df['distance_along_streakline'] = np.sqrt(squared_difference)
-    df.to_hdf(folder_path + filename, key='data', mode='w')
+    new_df['distance_along_streakline'] = np.sqrt(squared_difference)
+    new_df['odor']=df.odor
+    # new_df['windx']=df.windx
+    # new_df['windy']=df.windy
+    # new_df['x']=df.x
+    # new_df['y']=df.y
+    
+    new_df = new_df.iloc[100:]
+    new_df.reset_index(drop=True, inplace=True)
+    
+    fp='/home/beast/An/data/train_new_axis/'
+    new_df.to_hdf(fp + filename, key='data', mode='w')
 
-# Set folder path
-folder_path = '/home/beast/An/data/Sept13Plumes/plume2/train/'
-
-all_filenames = [filename for filename in os.listdir(folder_path) if filename.startswith("diag") and 
-                 filename.endswith(".h5")]
-# Sort filenames numerically
-sorted_filenames = sorted(all_filenames, key=lambda x: int(x[4:-3]))
 
 def main():
-    folder_path = '/home/beast/An/data/Sept13Plumes/plume2/train/'
+    folder_path = '/home/beast/An/data/train/'
     all_filenames = [filename for filename in os.listdir(folder_path) if filename.startswith("diag") and filename.endswith(".h5")]
     sorted_filenames = sorted(all_filenames, key=lambda x: int(x[4:-3]))
 
